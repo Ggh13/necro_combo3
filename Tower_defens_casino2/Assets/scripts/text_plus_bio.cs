487d0387c71c4d8718ce37d38d00b78e166e797b
Ggh13 <Roma_buzhor@mail.ru>
2022-07-27 22:53:46 +0300

    zero_start

--- Tower_defens_casino2/Assets/scripts/text_plus_bio.cs
+++ Tower_defens_casino2/Assets/scripts/text_plus_bio.cs
@@ -32,7 +32,7 @@ public class text_plus_bio : MonoBehaviour
     {
         text_bio.text = "+";
         text_bio.text = "+" + bio.ToString();
-        float t = 0.25f;
+        float t = 0f;
         while(t <= 1)
         {
             t += 0.000001f;
